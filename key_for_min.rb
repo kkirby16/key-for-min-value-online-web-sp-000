9abeaf995fc52f04c5f380358546276ff23f7961
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-15 21:35:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   min_value = 20000
   key_for_min_value = ""
+  
   name_hash.collect do |key, value|
     if value < min_value 
-    min_value = value 
-    key_for_min_value = key
+    min_value = value && key_for_min_value = key
   end
   key_for_min_value
     
