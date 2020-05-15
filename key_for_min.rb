d9954f793a052fb759c6e0fcb3dcf0a037b8ade4
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-15 21:47:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -17,8 +17,6 @@ def key_for_min_value(name_hash)
 end
 end
   key_for_min_value
-
-
 end
 
 #Track min_value and key_for_min_value. Iterate through the hash, and any time the #current value is lower than min_value, update both of these vars. At the end of the #loop, return key_for_min_value.
